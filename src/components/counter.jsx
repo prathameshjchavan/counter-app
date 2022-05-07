9ff18ea6f82085656916c3e1a2ac88f1b195e7b5
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-05-07 10:16:43 +0530

    added counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,18 +4,34 @@ class Counter extends Component {
 	state = {
 		count: 0,
 	};
+
+	handleIncrement = () => {
+		this.setState({ count: this.state.count + 1 });
+	};
+
 	render() {
 		return (
-			<React.Fragment>
-				<span>{this.formatCount()}</span>
-				<button>Increment</button>
-			</React.Fragment>
+			<div>
+				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+				<button
+					onClick={this.handleIncrement}
+					className="btn btn-secondary btn-sm"
+				>
+					Increment
+				</button>
+			</div>
 		);
+	}
+
+	getBadgeClasses() {
+		let classes = "badge text-black m-2 bg-";
+		classes += this.state.count === 0 ? "warning" : "primary";
+		return classes;
 	}
 
 	formatCount() {
 		const { count } = this.state;
-		return count === 0 ? <h1>Zero</h1> : count;
+		return count === 0 ? "Zero" : count;
 	}
 }
 
